6968c19ae2e49b659b99e0be8fc5bf6838946666
AlisUr <166662073+AlisaUrikh@users.noreply.github.com>
2025-11-07 23:57:26 +0300

    Create assert_expert.py

---
+++ homework 3/assert_expert.py
@@ -0,0 +1,34 @@
+from datetime import date
+from loader_2 import TextDataLoader
+
+loader = TextDataLoader()
+posts = loader.extract_posts()
+cleaned_posts = loader.clean_text(posts)
+
+"""
+Перепишите тесты для методов clean_text и extract_posts используя:
+
+- Assert с сообщениями об ошибках (2 любых теста)
+- Множественные проверки в одном assert (2 любых теста)
+- Создайте кастомную функцию, которая проверяет 2 теста сразу
+"""
+
+"""Задание 1"""
+assert len(posts)>0, "Список постов не должен быть пустым"
+assert all(cleaned_post['text'].islower() for cleaned_post in cleaned_posts), "В тексте постов не должно быть заглавных букв"
+
+"""Задание 2"""
+assert all('text' in post for post in posts) and all('date' in post for post in posts), "Ключи 'date' и 'text' обязательно должны быть в словаре"
+assert all('!' not in cleaned_post['text'] for cleaned_post in cleaned_posts) and all('?' not in cleaned_post['text'] for cleaned_post in cleaned_posts), "После обработки должны быть удалены все '!' и '?'"
+
+"""Задание 3"""
+def assert_text_extracted_and_cleaned():
+    """Кастомная функция проверки очистки текста"""
+    loader = TextDataLoader()
+    posts = loader.extract_posts()
+    cleaned_posts = loader.clean_text(posts)
+    d = date.today().strftime("%Y-%m-%d")
+    assert all(post['date'][:10]<d in post for post in posts), "Даты публикации постов должны быть раньше сегодняшней"
+    assert all('/n' not in cleaned_post['text'] for cleaned_post in cleaned_posts), "В очищенном тексте не должно быть переносов строк"
+
+assert_text_extracted_and_cleaned()
